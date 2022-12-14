f65ba8ed867e9f524c1adee98b6ad1c954fb5d97
Kanstantsin Yarmolenka <Kanstantsin_Yarmolenka@epam.com>
2022-12-14 13:33:08 +0300

    module2: fixed remarks: create separated variables for writable, readable streams and error callback.

--- task2.js
+++ task2.js
@@ -3,15 +3,14 @@ const fs = require("fs");
 const path = require("path");
 const { pipeline } = require("stream");
 
+const readStream = fs.createReadStream('csv/nodejs-hw1-ex1.csv');
+const writeStream = fs.createWriteStream(path.resolve(__dirname, "resultTask2.txt"));
+const errorHandler = (err) => err ? console.error('Pipeline failed.', err) : console.log('File was converted!');
+
+
 pipeline(
-    fs.createReadStream('csv/nodejs-hw1-ex1.csv'),
+    readStream,
     csv(),
-    fs.createWriteStream(path.resolve(__dirname, "resultTask2.txt")),
-    (err) => {
-        if (err) {
-            console.error('Pipeline failed.', err);
-        } else {
-            console.log('File was converted!');
-        }
-    },
+    writeStream,
+    errorHandler
 )
--- task3.js
+++ task3.js
@@ -3,15 +3,13 @@ import { pipeline } from 'stream'
 import csv from "csvtojson";
 import path from "path";
 
+const rs = fs.createReadStream('csv/nodejs-hw1-ex1.csv');
+const ws = fs.createWriteStream(path.resolve(__dirname, "resultTask3.txt"));
+const errorHandler = (err) => err ? console.error('Pipeline failed.', err) : console.log('File was converted!');
+
 pipeline(
-    fs.createReadStream('csv/nodejs-hw1-ex1.csv'),
+    rs,
     csv(),
-    fs.createWriteStream(path.resolve(__dirname, "resultTask3.txt")),
-    (err) => {
-        if (err) {
-            console.error('Pipeline failed.', err);
-        } else {
-            console.log('File was converted!');
-        }
-    },
+    ws,
+    errorHandler
 )
