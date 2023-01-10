cda2877104c4c6f76558637367e2c0fe1a2ba66c
Kanstantsin Yarmolenka <Kanstantsin_Yarmolenka@epam.com>
2023-01-10 21:53:09 +0300

    module#4: fixed remarks about awaits and with using `Promise.all` and return statement

--- module#2/src/scripts/fill-in-db-users.ts
+++ module#2/src/scripts/fill-in-db-users.ts
@@ -3,17 +3,13 @@ import { mockUsers } from "../mock";
 import { authenticateMessage } from "../constants";
 import { User } from "../models";
 
-const fillInDbUsers = async () => {
+(async () => {
   try {
-    await sequelize.authenticate();
+    await Promise.all([sequelize.authenticate(), User.sync({ force: true })]);
     console.info(authenticateMessage);
 
-    await User.sync({ force: true });
-
-    User.bulkCreate(mockUsers);
+    return User.bulkCreate(mockUsers);
   } catch (error) {
     console.log("Unable to connect to the database:", error);
   }
-};
-
-fillInDbUsers();
+})();
